086a9f49e9a001799740c2d1a701811bef0e80e9
huraicid <96579570+huraicid@users.noreply.github.com>
2023-01-18 22:21:26 +0900

    Update and rename Graph.cpp to main.cpp

---
+++ グラフ/main.cpp
@@ -0,0 +1,36 @@
+#include "Edge.h"
+#include "Graph.h"
+#include <vector>  
+#include <iostream>
+
+int main() {
+	// 頂点数と辺数
+	int N, M;
+	std::cin >> N >> M;
+
+	// グラフの初期化
+	Graph G(N);
+	for (int i = 0; i < N; i++) {
+		// 始点
+		int from;
+
+		// 行先
+		int to;
+
+		// 重み
+		long long w;
+
+		// 入力を受け付けて辺インスタンスを作成し格納
+		std::cin >> from >> to >> w;
+		G[from].push_back(Edge(to, w));
+	}
+
+	// グラフの中身を表示
+	GraphUtil::printAdjacencyList(G);
+	
+	return 0;
+}
+
+void search(const Graph& G, int s) {
+
+}
