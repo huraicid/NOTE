afcb5afb6110de02bb5e34580ef44eca56af8be0
huraicid <96579570+huraicid@users.noreply.github.com>
2023-02-03 23:54:39 +0900

    テストデータから自動的にグラフが生成されるよう修正

--- グラフ/testData/testDataGraph.py
+++ グラフ/testData/testDataGraph.py
@@ -1,34 +1,19 @@
 import graphviz
 
-# テストデータファイル読み込み
-f = open('testData.txt', 'r', encoding='UTF-8')
-
 # Graphvizのオブジェクトを作成 --- (*1)
-g = graphviz.Digraph(format='png', filename='test')
+g = graphviz.Digraph(format='png')
+g.attr('graph', rankdir='LR')
 
-# ノードを3つ作成 --- (*2)
-g.node('0')
-g.node('1')
-g.node('2')
-g.node('3')
-g.node('4')
-g.node('5')
-g.node('6')
-g.node('7')
-
-# ノードを接続する --- (*3)
-g.edge('0', '5')
-g.edge('1', '3')
-g.edge('1', '4')
-g.edge('1', '6')
-g.edge('2', '5')
-g.edge('2', '7')
-g.edge('3', '0')
-g.edge('3', '7')
-g.edge('4', '2')
-g.edge('4', '6')
-g.edge('6', '7')
-g.edge('7', '0')
+# テストデータファイル読み込み
+with open('testData.txt', 'r', encoding='UTF-8') as f:
+    for line in f:
+        targetData = line.split(' ')
+        # データ部のフォーマットにあっていない行は読み飛ばす
+        if len(targetData) != 3:
+            continue
+        # ノード、辺を追加
+        g.node(targetData[0])
+        g.edge(targetData[0], targetData[1], label=targetData[2].strip())
 
 # ファイル出力
 g.render('testData')
