8d9989c5f33c0d8a2829cd50c892f2a03dec8bb1
huraicid <shu.tateuchi@gmail.com>
2023-09-07 02:13:49 +0900

    2023/09/07

--- cardgame/dm/src/index.js
+++ cardgame/dm/src/index.js
@@ -1,6 +1,6 @@
 const COLUMN_DEFS = ["Deck Name", "Wins", "Loses", "WP"];
-const ROW_DATAS = [["ネバー・エンディング・ヒーロー", "0", "1", "0.0"],["ヘヴィ・デス・メタル", "1", "1", "0.5"],["ビクトリー・ソウル", "1", "1", "0.5"],["BATTLE of YAMATO魂", "2", "0", "1.0"],["Arcadias騎士団", "0", "1", "0.0"]];
-const COUNT = 4;
+const ROW_DATAS = [["ネバー・エンディング・ヒーロー", "0", "2", "0.0"],["ヘヴィ・デス・メタル", "1", "1", "0.5"],["ビクトリー・ソウル", "1", "1", "0.5"],["BATTLE of YAMATO魂", "2", "0", "1.0"],["Arcadias騎士団", "1", "1", "0.5"]];
+const COUNT = 5;
 
 new gridjs.Grid({
     sort: true,
