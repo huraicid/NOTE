e5790159a461332f3229ffb3a3c457e05cbf9935
huraicid <shu.tateuchi@gmail.com>
2023-09-18 12:04:22 +0900

    リファクタリング

--- cardgame/dm/culcurate.py
+++ cardgame/dm/culcurate.py
@@ -2,7 +2,6 @@ import csv
 import math
 import re
 
-
 # カラムのindex定義
 DECK1_COL_NO = 1
 DECK2_COL_NO = 2
@@ -12,8 +11,39 @@ WIN_COL_NO = 3
 count = 0
 aggregatedData = {}
 
+# main部
+def main():
+    readResultData()
+    modifyIndexJsData()
+
+# csvファイルから戦績データを読み込み集計データを作成する
+def readResultData():
+    with open('./data/results.csv') as f:
+        # csvファイルを読み込む
+        reader = csv.reader(f)
+        rows = [row for row in reader]
+
+        # 対戦回数（データ数）を計算
+        # ヘッダ行を除いた行数
+        global count
+        count = len(rows) - 1
+
+        # 1行目はヘッダ行なので読み込まない
+        for row in rows[1:]:
+            # 集計対象のデッキが戦績集計データ内に存在しなければ初期化
+            if row[DECK1_COL_NO] not in aggregatedData:
+                aggregatedData[row[DECK1_COL_NO]] = {'wins': 0, 'loses': 0, 'wp': 0}
+            if row[DECK2_COL_NO] not in aggregatedData:
+                aggregatedData[row[DECK2_COL_NO]] = {'wins': 0, 'loses': 0, 'wp': 0}
+            
+            # 勝ち負けデータを読み込み更新する
+            if row[WIN_COL_NO] == row[DECK1_COL_NO]:
+                updateAggregatedData(row, DECK1_COL_NO, DECK2_COL_NO)
+            else:
+                updateAggregatedData(row, DECK2_COL_NO, DECK1_COL_NO)
+
 # 戦績データを更新する
-def updateAggregatedData(winnerCol, loserCol):
+def updateAggregatedData(row, winnerCol, loserCol):
     aggregatedData[row[winnerCol]]['wins'] += 1
     winnerWpNotRounded = aggregatedData[row[winnerCol]]['wins'] / (aggregatedData[row[winnerCol]]['wins'] + aggregatedData[row[winnerCol]]['loses'])
     aggregatedData[row[winnerCol]]['wp'] = math.floor(winnerWpNotRounded * 10 ** 2) / (10 ** 2)
@@ -21,33 +51,6 @@ def updateAggregatedData(winnerCol, loserCol):
     aggregatedData[row[loserCol]]['loses'] += 1
     loserWpNotRounded = aggregatedData[row[loserCol]]['wins'] / (aggregatedData[row[loserCol]]['wins'] + aggregatedData[row[loserCol]]['loses'])
     aggregatedData[row[loserCol]]['wp'] = math.floor(loserWpNotRounded * 10 ** 2) / (10 ** 2)
-
-
-# デッキ名の略称を日本語の正式名称に変換する
-def getJpnDeckName(name):
-    if name == 'bs':
-        return 'DMC-39 ビクトリー・ソウル'
-    elif name == 'hdm':
-        return 'DMC-40 ヘヴィ・デス・メタル'
-    elif name == 'neh':
-        return 'DMC-43 ネバー・エンディング・ヒーロー'
-    elif name == 'ymt':
-        return 'DMC-45 BATTLE of YAMATO魂'
-    elif name == 'ngt':
-        return 'DMC-46 Arcadias騎士団'
-    else:
-        return 'Undefined'
-
-
-# 戦績集計データのデッキ名部分を略称から日本語の正式名称に修正したものを取得する
-def getJpnNameAggregatedData():
-    tmpAggregatedData = {}
-    for key in aggregatedData:
-        newKey = getJpnDeckName(key)
-        tmpAggregatedData[newKey] = aggregatedData[key]
-    
-    return tmpAggregatedData
-
 
 # index.jsのデータ部分を書き換える
 def modifyIndexJsData():
@@ -74,26 +77,30 @@ def modifyIndexJsData():
     with open('./src/index.js', mode='w', encoding='utf-8') as js:
         js.write(newContent)
 
+# 戦績集計データのデッキ名部分を略称から日本語の正式名称に修正したものを取得する
+def getJpnNameAggregatedData():
+    tmpAggregatedData = {}
+    for key in aggregatedData:
+        newKey = getJpnDeckName(key)
+        tmpAggregatedData[newKey] = aggregatedData[key]
+    
+    return tmpAggregatedData
 
-# csvファイルを読み込む
-with open('./data/results.csv') as f:
-    reader = csv.reader(f)
-    # 1行目はヘッダ行なので読み込まない
-    rows = [row for row in reader]
-    for row in rows[1:]:
-        # 集計対象のデッキが戦績集計データ内に存在しなければ初期化
-        if row[DECK1_COL_NO] not in aggregatedData:
-            aggregatedData[row[DECK1_COL_NO]] = {'wins': 0, 'loses': 0, 'wp': 0}
-        if row[DECK2_COL_NO] not in aggregatedData:
-            aggregatedData[row[DECK2_COL_NO]] = {'wins': 0, 'loses': 0, 'wp': 0}
-        
-        # 勝ち負けデータを読み込み更新する
-        if row[WIN_COL_NO] == row[DECK1_COL_NO]:
-            updateAggregatedData(DECK1_COL_NO, DECK2_COL_NO)
-        else:
-            updateAggregatedData(DECK2_COL_NO, DECK1_COL_NO)
-        
-        # 対戦回数を加算
-        count += 1
+# デッキ名の略称を日本語の正式名称に変換する
+def getJpnDeckName(name):
+    if name == 'bs':
+        return 'DMC-39 ビクトリー・ソウル'
+    elif name == 'hdm':
+        return 'DMC-40 ヘヴィ・デス・メタル'
+    elif name == 'neh':
+        return 'DMC-43 ネバー・エンディング・ヒーロー'
+    elif name == 'ymt':
+        return 'DMC-45 BATTLE of YAMATO魂'
+    elif name == 'ngt':
+        return 'DMC-46 Arcadias騎士団'
+    else:
+        return 'Undefined'
 
-    modifyIndexJsData()
+
+if __name__ == '__main__':
+    main()
